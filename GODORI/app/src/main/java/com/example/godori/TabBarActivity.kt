6b70c860676d5055ca0821c87b1a98f13674d39d
Song_a <o37o1108@gmail.com>
2020-11-24 20:20:57 +0900

    [add] 탭바작업중

---
+++ GODORI/app/src/main/java/com/example/godori/TabBarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.godori
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TabBarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_bar)
+    }
+}
