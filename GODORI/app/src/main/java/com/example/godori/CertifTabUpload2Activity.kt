053b719789c202ba389faaf91b1be788a7525f25
Song_a <o37o1108@gmail.com>
2020-12-10 03:28:21 +0900

    [ADD]피드백 수정 사항

--- GODORI/app/src/main/java/com/example/godori/CertifTabUpload2Activity.kt
+++ GODORI/app/src/main/java/com/example/godori/CertifTabUpload2Activity.kt
@@ -8,6 +8,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_certif_tab_upload1.*
 import kotlinx.android.synthetic.main.activity_certif_tab_upload2.*
 
 
@@ -22,34 +23,44 @@ class CertifTabUpload2Activity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        Btn3.setOnTouchListener { view, motionEvent ->
+        Btn4.setOnTouchListener { view, motionEvent ->
             if (motionEvent.action == MotionEvent.ACTION_DOWN) {
                 //터치했을 때
-                Btn3.setBackgroundResource(R.drawable.button_circle2)
+                Btn4.setBackgroundResource(R.drawable.button_circle2)
+                Btn4.setTextColor(Color.WHITE)
             } else if (motionEvent.action == MotionEvent.ACTION_UP) {
                 //터치 해제했을 때
-                Btn3.setBackgroundResource(R.drawable.button_circle2)
+                Btn4.setBackgroundResource(R.drawable.button_circle2)
+                Btn4.setTextColor(Color.WHITE)
             }
             false
         }
 
-        ImgRun.setOnTouchListener { view, motionEvent ->
-            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
-                //터치했을 때
-                ImgRun.setImageResource(R.drawable.img_running_m_blue)
-            } else if (motionEvent.action == MotionEvent.ACTION_UP) {
-                //터치 해제했을 때
-                ImgRun.setImageResource(R.drawable.img_running_m_blue)
-            }
-            false
-        }
         imgHealth.setOnTouchListener { view, motionEvent ->
             if (motionEvent.action == MotionEvent.ACTION_DOWN) {
                 //터치했을 때
                 imgHealth.setImageResource(R.drawable.img_health_blue)
+                imgHealth.setBackgroundResource(R.drawable.button_circle2)
+                imgHealthText.setTextColor(Color.parseColor("#677BF0"))
             } else if (motionEvent.action == MotionEvent.ACTION_UP) {
                 //터치 해제했을 때
                 imgHealth.setImageResource(R.drawable.img_health_blue)
+                imgHealth.setBackgroundResource(R.drawable.button_circle2)
+                imgHealthText.setTextColor(Color.parseColor("#677BF0"))
+            }
+            false
+        }
+        imgYoga.setOnTouchListener { view, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                //터치했을 때
+                imgYoga.setImageResource(R.drawable.img_yoga_blue)
+                imgYoga.setBackgroundResource(R.drawable.button_circle2)
+                imgYogaText.setTextColor(Color.parseColor("#677BF0"))
+            } else if (motionEvent.action == MotionEvent.ACTION_UP) {
+                //터치 해제했을 때
+                imgYoga.setImageResource(R.drawable.img_yoga_blue)
+                imgYoga.setBackgroundResource(R.drawable.button_circle2)
+                imgYogaText.setTextColor(Color.parseColor("#677BF0"))
             }
             false
         }
