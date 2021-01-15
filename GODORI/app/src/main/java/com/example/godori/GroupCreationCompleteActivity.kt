e98b50b1c4338a35d045555711101dcc0745d7d8
gineepark <bluejihnee@gmail.com>
2021-01-16 02:42:09 +0900

    그룹 생성 플로우(GroupCreation) 뷰3개 추가, 그룹 생성/가입 완료 뷰

---
+++ GODORI/app/src/main/java/com/example/godori/GroupCreationCompleteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.godori
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupCreationCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_creation_complete)
+    }
+}
---
+++ GODORI/app/src/main/java/com/example/godori/GroupJoinCompleteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.godori
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupJoinCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_join_complete)
+    }
+}
